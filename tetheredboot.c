fb165f0df697073a5ecf1f3c20a1d3490de82db7
msftguy <msft.guy@gmail.com>
2010-11-20 14:34:09 -0800

    Added getopt.c for win32 build; added an option to load ramdisk.

--- tetheredboot.c
+++ tetheredboot.c
@@ -1,27 +1,36 @@
 /**
-  * GreenPois0n Syringe - injectpois0n.c
-  * Copyright (C) 2010 Chronic-Dev Team
-  * Copyright (C) 2010 Joshua Hill
-  *
-  * This program is free software: you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License as published by
-  * the Free Software Foundation, either version 3 of the License, or
-  * (at your option) any later version.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  * GNU General Public License for more details.
-  *
-  * You should have received a copy of the GNU General Public License
-  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- **/
+* GreenPois0n Syringe - injectpois0n.c
+* Copyright (C) 2010 Chronic-Dev Team
+* Copyright (C) 2010 Joshua Hill
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
 
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <getopt.h>
 
 #include "libpois0n.h"
+
+#include <sys/stat.h>
+
+bool file_exists(const char* fileName)
+{
+   struct stat buf;
+   return !stat(fileName, &buf);
+}
 
 void print_progress(double progress, void* data) {
 	int i = 0;
@@ -50,18 +59,59 @@ void print_progress(double progress, void* data) {
 
 void usage()
 {
-	printf("Usage: tetheredboot <ibss> <kernelcache>\n");
+	printf("Usage: tetheredboot [-h]|[-v][-i <ibss> [-k <kernelcache>][-r <ramdisk>]]\n");
 	exit(0);
 }
 
+bool g_verbose = false;
+
 int main(int argc, char* argv[]) {
 	int result = 0;
-	if (argc != 3) {
-		usage();
+	irecv_error_t ir_error = IRECV_E_SUCCESS;
+
+	//int index;
+	const char 
+		*ibssFile = NULL,
+		*kernelcacheFile = NULL,
+		*ramdiskFile = NULL;
+	int c;
+
+	opterr = 0;
+
+	while ((c = getopt (argc, argv, "vhi:k:r:")) != -1)
+		switch (c)
+	{
+		case 'v':
+			g_verbose = true;
+			break;
+		case 'h':
+			usage();
+			break;
+		case 'i':
+			if (!file_exists(optarg)) {
+				error("Cannot open iBSS file '%s'", optarg);
+				return -1;
+			}
+			ibssFile = optarg;
+			break;
+		case 'k':
+			if (!file_exists(optarg)) {
+				error("Cannot open kernelcache file '%s'", optarg);
+				return -1;
+			}
+			kernelcacheFile = optarg;
+			break;
+		case 'r':
+			if (!file_exists(optarg)) {
+				error("Cannot open ramdisk file '%s'", optarg);
+				return -1;
+			}
+			ramdiskFile = optarg;
+			break;
+		default:
+			usage();
 	}
-	const char* ibssFile = argv[1];
-	const char* kernelcacheFile = argv[2];
-	
+
 	pois0n_init();
 	pois0n_set_callback(&print_progress, NULL);
 
@@ -76,37 +126,61 @@ int main(int argc, char* argv[]) {
 		error("Your device in incompatible with this exploit!\n");
 		return result;
 	}
-	
+
 	result = pois0n_injectonly();
 	if (result < 0) {
 		error("Exploit injection failed!\n");
 		return result;
 	}
-	
-	debug("Uploading %s to device\n", ibssFile);
-	irecv_error_t error = irecv_send_file(client, ibssFile, 1);
-	if(error != IRECV_E_SUCCESS) {
-		error("Unable to upload iBSS\n");
-		debug("%s\n", irecv_strerror(error));
-		return -1;
+
+	if (ibssFile != NULL) {
+		debug("Uploading %s to device\n", ibssFile);
+		ir_error = irecv_send_file(client, ibssFile, 1);
+		if(ir_error != IRECV_E_SUCCESS) {
+			error("Unable to upload iBSS\n");
+			debug("%s\n", irecv_strerror(ir_error));
+			return -1;
+		}
+	} else {
+		return 0;
 	}
-	
+
 	client = irecv_reconnect(client, 10);
-	
-	debug("Uploading %s to device\n", kernelcacheFile);
-	error = irecv_send_file(client, kernelcacheFile, 1);
-	if(error != IRECV_E_SUCCESS) {
-		error("Unable to upload kernelcache\n");
-		debug("%s\n", irecv_strerror(error));
-		return -1;
+
+	if (ramdiskFile != NULL) {
+		debug("Uploading %s to device\n", ramdiskFile);
+		ir_error = irecv_send_file(client, ramdiskFile, 1);
+		if(ir_error != IRECV_E_SUCCESS) {
+			error("Unable to upload ramdisk\n");
+			debug("%s\n", irecv_strerror(ir_error));
+			return -1;
+		}
+
+		sleep(5);
+
+		ir_error = irecv_send_command(client, "ramdisk");
+		if(ir_error != IRECV_E_SUCCESS) {
+			error("Unable send the bootx command\n");
+			return -1;
+		}	
 	}
 	
-	error = irecv_send_command(client, "bootx");
-	if(error != IRECV_E_SUCCESS) {
-		error("Unable send the bootx command\n");
-		return -1;
+	if (kernelcacheFile != NULL) {
+		debug("Uploading %s to device\n", kernelcacheFile);
+		ir_error = irecv_send_file(client, kernelcacheFile, 1);
+		if(ir_error != IRECV_E_SUCCESS) {
+			error("Unable to upload kernelcache\n");
+			debug("%s\n", irecv_strerror(ir_error));
+			return -1;
+		}
+
+		ir_error = irecv_send_command(client, "bootx");
+		if(ir_error != IRECV_E_SUCCESS) {
+			error("Unable send the bootx command\n");
+			return -1;
+		}
 	}
-	
+
 	pois0n_exit();
 	return 0;
 }
